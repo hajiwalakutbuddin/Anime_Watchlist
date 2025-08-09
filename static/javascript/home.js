aa8a9638ff90dc5aa7e6de92cca6b06c10b95ba5
hajiwalakutbuddin <hajiwalakutbuddin@gmail.com>
2025-08-09 14:09:39 +0530

    1

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -1,10 +1,11 @@
-        document.addEventListener('DOMContentLoaded', function() {
-            function formatInput(e) {
-                let value = e.target.value.toUpperCase().replace(/ /g, '_');
-                e.target.value = value;
-            }
-            var addInput = document.getElementById('add-i');
-            var deleteInput = document.getElementById('delete-i');
-            if(addInput) addInput.addEventListener('input', formatInput);
-            if(deleteInput) deleteInput.addEventListener('input', formatInput);
-        });
+
+document.addEventListener('DOMContentLoaded', function() {
+    function formatInput(e) {
+        let value = e.target.value.toUpperCase().replace(/ /g, '_');
+        e.target.value = value;
+        }
+    var addInput = document.getElementById('add-anime');
+    var deleteInput = document.getElementById('delete-anime');
+    if(addInput) addInput.addEventListener('input', formatInput);
+    if(deleteInput) deleteInput.addEventListener('input', formatInput);
+});
